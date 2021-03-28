9ae41c7cf407f646d0e6a9ec998a128a5704d309
Kevin Zhu <kenblu24@hotmail.com>
2021-03-28 10:15:39 -0400

    fixed boot.py

--- micropython_root/boot.py
+++ micropython_root/boot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file is executed on every boot (including wake-boot from deepsleep)
 import gc
 from sys import modules
@@ -8,49 +7,7 @@ from machine import Pin, I2C
 # import webrepl
 # webrepl.start()
 
-
-def start_ap():
-    import network
-    import webrepl
-
-    wlan = network.WLAN(network.STA_IF)  # create station interface
-    wlan.active(True)       # activate the interface
-    wlan.scan()             # scan for access points
-    wlan.isconnected()      # check if the station is connected to an AP
-    wlan.connect('Helium', '#algorithmicBEETROOT')  # connect to an AP
-    wlan.config('mac')      # get the interface's MAC address
-    wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
-
-    ap = network.WLAN(network.AP_IF)  # create access-point interface
-    ap.config(essid='NSA_Surveilance_Van')  # set the ESSID of the access point
-    ap.config(max_clients=3)  # set how many clients can connect to the network
-    ap.active(True)         # activate the interface
-
-    webrepl.start()
-
-
-
-
-def do_connect():
-    import network
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect('Helium', '#algorithmicBEETROOT')
-        i = 0
-        while not wlan.isconnected():
-            if i > 5:
-                i = i + 1
-                return
-    print('network config:', wlan.ifconfig())
-
-
-# do_connect()
-# start_ap()
-
 i2c = I2C(0, scl=Pin(22), sda=Pin(21))
-=======
 
 
 def reload(mod):
@@ -61,7 +18,7 @@ def reload(mod):
 
 
 def start_ap():
-import network
+    import network
     import webrepl
 
     ap = network.WLAN(network.AP_IF)
@@ -73,4 +30,3 @@ import network
 
 
 start_ap()
->>>>>>> origin/master
