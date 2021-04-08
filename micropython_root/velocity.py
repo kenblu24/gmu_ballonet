449975b59e5704dffa2d169fff313eacb36e1bcd
keckertgmu <80475868+keckertgmu@users.noreply.github.com>
2021-04-08 12:02:43 -0400

    Add files via upload

---
+++ micropython_root/velocity.py
@@ -0,0 +1,50 @@
+"""
+Authors: Kevin Eckert
+This controller is a simple controller intended to be used 
+with the bno055.py library. 
+
+Using quaternion rotation, it attempts to calculate motion 
+due to movement and uses that to calculate the current velocity. 
+
+"""
+
+import bno055 as bno
+from time import sleep
+import pump
+
+ctrl = 1
+delta = 0.6
+
+def velocity(i2c,address):
+	velocity = 0
+	bno.set_mode(i2c,address)
+	sleep(1)
+	while(ctrl==1):
+		matrix = bno.get_quaternion_data(i2c,address)
+		if velocity != 0:
+			accel = bno.get_linear_accel_data(i2c,address)
+			accel = bno.quaternion_rotation(matrix,accel)
+			sleep(delta)
+			velocity = bno.calculate_velocity(velocity,accel,delta)
+			if velocity < 0:
+				pump.pump_out()
+			if velocity > 0:
+				pump.pump_in()
+		if velocity == 0:
+			pump.stop()
+	return 0
+	
+def stop():
+	ctrl = 0
+	pump.stop()
+	return 0
+	
+def start():
+	ctrl = 1
+	return 0
+	
+def set_sample_rate(x):
+	delta = x
+	return 0
+
+
